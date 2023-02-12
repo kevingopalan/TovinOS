9ebfba6e0ae9c0e484627ce0f4c31d65e6cf068f
kevingopalan <kevin.gopalan@vestal.k12.ny.us>
2023-02-12 18:09:13 +0000

    Edited settings-new.php

--- settings-new.php
+++ settings-new.php
@@ -1,1 +1,14 @@
+<style>
+.settings-actions div {
+  margin: 16px;
+  display: inline-block !important;
+  text-align: center;
+}
+</style>
 <h1>Settings</h1>
+<button onclick = "showWindow(11, 'settings-new.php')">Relaunch</button><br>
+<div class = "settings-actions">
+  <div><img src = "img/wow64/64x64/apps/com.github.maoschanz.DynamicWallpaperEditor.svg"><br>Personalize</div>
+  <div><img src = "img/wow64/64x64/apps/system-users.svg"><br>Users</div>
+  <div><img src = "img/wow64/64x64/apps/system-users.svg"><br>Network</div>
+</div>
