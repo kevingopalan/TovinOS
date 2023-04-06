516929144a18bf3b6543d87338e7420cb7c9161a
kevingopalan <kevin.gopalan@vestal.k12.ny.us>
2023-04-06 21:06:15 +0000

    Changed preloader style

--- preloader.php
+++ preloader.php
@@ -1,4 +1,4 @@
 <div class="loader" id="load">
-  <img src = "img/TovinOS.png" width = "300px"/>
+  <img src = "img/appmenu.png" width = "120px"/>
   <div class = "loadtoolong">Seems like TovinOS is taking long to load. Try using another computer/browser</div>
 </div>
