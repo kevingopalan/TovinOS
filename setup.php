743773a0460a909c0e39e5892974b4036e70866d
kevingopalan <kevin.gopalan@vestal.k12.ny.us>
2023-06-24 16:15:19 +0000

    edit setup

--- setup.php
+++ setup.php
@@ -2,7 +2,7 @@
   <h1>Welcome</h1>
   <form onsubmit = "return false">
     <h2>Security</h2>
-    <input type = password id = "submitpwd" required>
+    <input type = password id = "submitpwd" placeholder = "Please enter your password" required>
     <br>
     <label style = "display: none;"><input type="checkbox" id="myChecki" onclick="darklight2()"/> Dark Mode</label>
     <br>
